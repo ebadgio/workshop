0a337fb9d3e5a95745843d16154f8c7a04d27ad4
ebadgio <eb27@rice.edu>
2018-08-03 15:23:22 -0400

    user model

--- server/models/User.js
+++ server/models/User.js
@@ -33,6 +33,10 @@ const userSchema = new mongoose.Schema({
         type: String,
         default: ''
     },
+    profilePicture: {
+        type: String,
+        default: ''
+    }
     bio: {
         type: String,
         default: ''
